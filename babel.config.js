a07240978a7337906f6353bcce8a998d89484722
Daniel Reabovetchi <redarkov@gmail.com>
2025-03-24 19:01:15 +0200

    Fix reanimated plugin in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,7 @@ module.exports = {
   presets: [
     ['module:react-native-builder-bob/babel-preset', { modules: 'commonjs' }],
   ],
+  plugins: [
+      'react-native-reanimated/plugin',
+    ],
 };
